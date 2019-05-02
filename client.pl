2d876b14d69a24926b38e77ab7e780655924b950
Santiago Zarate <santiago@zarate.co>
2019-05-02 12:49:05 +0200

    Update client.pl

--- client.pl
+++ client.pl
@@ -7,6 +7,6 @@ BEGIN { unshift @INC, "$FindBin::Bin/lib" }
 use Alexandria::Client;
 use feature 'say';
 
-my $url = $ARGV[0] // 'https://freeditorial.com/es/books/la-guerra-de-los-mundos';
+my $url = $ARGV[0] // $default_url; # this should be a Mojo::URL instance
 my $downloader = Alexandria::Client->new(url => $url);
 $downloader->process;
